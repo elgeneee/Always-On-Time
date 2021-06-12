33fd6be68f60defeb43b9e7ce1ef9336d4e24de2
elgene <eedren@gmail.com>
2021-06-12 19:23:00 +0800

    added documentation

--- src/sample/Graph.java
+++ src/sample/Graph.java
@@ -1,8 +1,17 @@
 package sample;
+
 import java.time.Duration;
 import java.time.Instant;
 import java.util.*;
 
+/**
+ * The {@code Graph} class represents a data structure that is connected by Customers and Depot. It extends class {@code Thread} that aims to perform parallelism.
+ * <p>
+ * When a Graph is first created, it calculates the distance between each adjacent {@code Location} and store in the adjacency matrix
+ *
+ * @author  Elgene Ee, Chong Wen, Chiao Yun, Boon Guan
+ * @since   1.0
+ */
 public class Graph extends Thread{
     double adjMatrix[][];
     int numOfVehicles;
@@ -16,7 +25,16 @@ public class Graph extends Thread{
     double routeCost;
     double tourCost;
 
-    //params for MCTS
+    /**
+     * param for DFS
+     */
+    ArrayList<Integer> bestPath;
+    double bestCost;
+    Instant startTime;
+
+    /**
+     * param for MCTS
+     */
     int N;
     int ALPHA;
     double z;
@@ -26,9 +44,20 @@ public class Graph extends Thread{
     Tour best_tour = new Tour(Double.POSITIVE_INFINITY);
     int timeLimit = 60;
 
-    //params for threading
+    /**
+     * param for Threading
+     */
     Component com = Component.getInstance();
 
+    /**
+     * Constructs a graph by connecting all nodes between each other.
+     * <p>
+     * The distance between each node is stored into a 2D array, {@code adjMatrix}
+     * <p>
+     * Note that the parameters for MCTS algorithm is initialized as well
+     *
+     * @param   c  list of locations
+     */
     public Graph(ArrayList<Location> c){
         this.c = c;
         d=(Depot)c.get(0);
@@ -43,222 +72,29 @@ public class Graph extends Thread{
             for (int j = 0; j < c.size(); j++) {
                 if(i==j) continue;
                 if(adjMatrix[i][j]!=0)
-                    continue; //because save before
+                    continue;
                 dist = Math.sqrt(Math.pow(c.get(i).xCoordinate-c.get(j).xCoordinate,2) + Math.pow(c.get(i).yCoordinate-c.get(j).yCoordinate,2));
 
-                adjMatrix[i][j] = adjMatrix[j][i] = dist;  //because is undirected edge
+                adjMatrix[i][j] = adjMatrix[j][i] = dist;
             }
         }
         location = (ArrayList<Location>) c.clone();
     }
 
-//    public void displayEdges(){
-//        for (int i = 0; i < adjMatrix.length; i++) {
-//            for (int j = 0; j < adjMatrix.length; j++) {
-//                System.out.print(adjMatrix[i][j] + " ");
-//            }
-//            System.out.println();
-//        }
-//    }
-//
-//    public int getAdjUnvisitedVertex(int v){
-//        for (int i = 1; i < c.size(); i++) {
-//            Customer cus=(Customer)c.get(i);  //Always need to downcast
-//            if(!cus.wasVisited && adjMatrix[v][i]>0){
-//                return c.get(i).id;
-//            }
-//        }
-//        return -1;
-//    }
-
-
+    /**
+     * Performs {@code MCTS} algorithm when a thread is started
+     */
     @Override
     public void run() {
         com.clearMCTSTour();
         com.setMctsTour(mctsSearch(3,100));
     }
 
-    public ArrayList<Vehicle> bfs2(){
-        List<Integer> adjList[]= new LinkedList[c.size()-1];  //adjacency list as different route combination
-        double [] cost=new double[c.size()-1];      //cost of the adjList[i] route
-        int [] accumulatedSize=new int[c.size()-1];
-
-        boolean [][] visited = new boolean[c.size()-1][c.size()-1];
-        //store visited customer for every customer
-        //2D array because for each adjList, every customer must be visited
-
-        for (int i = 0; i < adjList.length; i++) {
-            adjList[i]=new LinkedList<>();
-            adjList[i].add(0);   //every route will start from depot
-        }
-
-        for (int i = 0; i < adjList.length; i++) { //bfs first level
-            adjList[i].add(i + 1);  //add every possible customer as first visit
-            cost[i] += adjMatrix[0][i + 1];  //depot to first customer
-            accumulatedSize[i]+=c.get(i+1).demandSize;
-            visited[i][i]=true;  //first node visited
-        }
-        while(!completeArrayVisited(visited)) {  //everytime start this line means a new level of bfs
-            int x;
-
-            for (int i = 0; i < adjList.length; i++) {
-                x=adjList[i].get(adjList[i].size()-1);  //get last element of list
-                int minNode=0;
-                double min=Double.POSITIVE_INFINITY;
-                for (int j = 1; j < adjMatrix.length; j++) {  //check distance to adjacent node of customer i
-                    if(visited[i][j-1])  //visited node in the list
-                        continue;
-                    if(adjMatrix[x][j]<min && accumulatedSize[i]+c.get(j).demandSize<= d.maximumCapacity ) {
-                        min = adjMatrix[x][j];  //update the route with cheapest cost
-                        minNode=j;
-                    }
-
-                }
-                if(minNode!=0 ) {  //there exists a customer demand size that still can be add to the vehicle
-                    cost[i] += min;
-                    adjList[i].add(minNode);
-                    accumulatedSize[i] += c.get(minNode).demandSize;
-                    visited[i][minNode-1]=true;
-                }
-                else{  //no customer demand size can fit in the vehicle anymore, need new vehicle
-                    accumulatedSize[i]=0;
-                    cost[i] += adjMatrix[adjList[i].get(adjList[i].size()-1)][0]; //go back to depot
-                    adjList[i].add(0);   //act as ending for previous route and also starting of next vehicle
-
-                }
-            }
-            if(completeArrayVisited(visited)) {  //every customer visited, now return to depot and terminate
-                for (int i = 0; i < adjList.length; i++) {
-                    if(adjList[i].get(adjList[i].size()-1)!=0) {
-                        cost[i] += adjMatrix[adjList[i].get(adjList[i].size() - 1)][0];
-                        adjList[i].add(0);
-                    }
-                }
-                break;
-            }
-        }
-
-        double minCost=Double.POSITIVE_INFINITY;
-        int minIndex=0;
-        for (int i = 0; i < adjList.length; i++) {
-            if(cost[i]<minCost){
-                minCost=cost[i];
-                minIndex=i;
-            }
-        }
-
-
-        int index=1;
-        linkedList.clear();
-        int  currentLoad=0;
-        linkedList.add(c.get(0));
-        for (int i = index; i < adjList[minIndex].size(); i++) {  //separate the minCost path into different vehicle
-            if(adjList[minIndex].get(i) != 0) {
-                linkedList.add(c.get(adjList[minIndex].get(i)));
-                currentLoad+=c.get(adjList[minIndex].get(i)).demandSize;
-                continue;
-            }
-            index=i;
-            linkedList.add(c.get(0));
-            routeCost = computeRouteCost(linkedList);
-            vehicleList.add(new Vehicle(linkedList, routeCost,currentLoad));
-            linkedList.clear();
-            currentLoad=0;
-            linkedList.add(c.get(0));
-        }
-        sb = new StringBuilder();
-        sb.append("Basic Simulation Tour\nTour Cost: " + minCost + "\n");
-        displayVehicle2(vehicleList);
-
-        resetVisited();
-        return vehicleList;
-
-
-    }
-
-    public boolean completeArrayVisited(boolean [] [] visited){
-        for (int i = 0; i < visited.length; i++){
-            for(int j = 0; j <visited[0].length; j++) {
-                if (!visited[i][j])
-                    return false; //still have unvisited node
-
-            }
-        }
-        return true;
-    }
-
-//    public void bfs() {
-//        double shortestPath;
-//        int currentLoad;
-//        tourCost=0;
-//
-//        q.add(1); //start from 1 , because Customer class start from index 1
-//        int v1=q.peek();//take the first customer
-//        int v2; //check the nodes adjacent to it, since this is a complete undirected graph, the nodes adjacent to customer 1 is 2,3,4
-//        while((v2=getAdjUnvisitedVertex(v1))!=-1){ //Queue = {1,2,3,4}
-//            ((Customer)(c.get(v2))).wasVisited=true;
-//            q.add(v2);
-//        }
-//
-//        for (int i = 1; i < c.size(); i++) { //resetting all to false to create connections later
-//            ((Customer)(c.get(i))).wasVisited=false;
-//        }
-//
-//
-//        while(!completeVisited()){
-//            linkedList.clear();
-//            linkedList.add (c.get(0)); //add depot
-//            currentLoad=0;
-//            shortestPath=Double.POSITIVE_INFINITY;
-//            if( ((Customer)c.get(q.peek())).wasVisited){
-//                q.remove();
-//            }else{
-//                v2 = q.remove();
-//                ((Customer)c.get(v2)).wasVisited = true;
-//                currentLoad+=c.get(v2).demandSize;
-//                linkedList.add (c.get(v2));  //linkedList for the vehicle
-//                int temp=0; //customer ID
-//                for (int i = 1; i < c.size(); i++) { //create a temporary shortest path here (e.g. 1->2, we know later it will be replaced by 3)
-//                    Customer cus=(Customer) c.get(i);
-//                    if (!cus.wasVisited && (currentLoad + cus.demandSize) <= d.maximumCapacity) {  //d.demandSize= depot MaximumCapacity
-//                        shortestPath = adjMatrix[v2][i];
-//                        temp = i;
-//                        break;
-//                    }
-//                }
-//                for (int k = 1; k < c.size(); k++) { //find the shortest path
-//                    Customer cus=(Customer) c.get(k);
-//                    if (!cus.wasVisited && (currentLoad + cus.demandSize) <= d.maximumCapacity) {
-//                        if(shortestPath > adjMatrix[v2][k]){
-//                            shortestPath = adjMatrix[v2][k];
-//                            temp = k;
-//                        }
-//                    }
-//                }
-//                if(temp==0){//temp==0 means there is only one node(e.g. 0->4->0)
-//                    linkedList.add (c.get(0)); //add depot
-//                    routeCost = computeRouteCost(linkedList);
-//                    vehicleList.add(new Vehicle(linkedList, routeCost,currentLoad));
-//                }else{//(e.g. 0->1->3->0)
-//                    Customer cus=(Customer)c.get(temp);
-//                    cus.wasVisited=true;
-//                    currentLoad+=cus.demandSize;
-//                    linkedList.add(cus);
-//                    linkedList.add (c.get(0)); //add depot
-//                    routeCost = computeRouteCost(linkedList);
-//                    vehicleList.add(new Vehicle(linkedList, routeCost,currentLoad));
-//                }
-//                tourCost+=routeCost;
-//
-//            }
-//        }
-//
-//        //display output
-//        System.out.println("Basic Simulation Tour" + "\nTour Cost: " + tourCost);
-//        displayVehicle(vehicleList);
-//    }
-
-
+    /**
+     * Tests if all locations are visited
+     *
+     * @return  {@code true} if and only if all locations are visited, {@code false} otherwise.
+     */
     public boolean completeVisited(){
         for (int i = 1; i < c.size(); i++) {
             if(!((Customer)c.get(i)).wasVisited) return false;
@@ -266,6 +102,12 @@ public class Graph extends Thread{
         return true;
     }
 
+    /**
+     * Returns the route cost taken based on the linked list provided
+     *
+     * @param   linkedList   the location LinkedList.
+     * @return  the route cost for a given route
+     */
     public double computeRouteCost(LinkedList<Location> linkedList){
         double routeCost=0;
 
@@ -277,23 +119,24 @@ public class Graph extends Thread{
         return routeCost;
     }
 
-    public void displayVehicle(ArrayList<Vehicle> vehicleList){
-        for (int i = 1; i <= vehicleList.size(); i++) {
-            System.out.println("Vehicle " + i);
-            System.out.println(vehicleList.get(i-1));
-        }
-    }
-
-    public String displayVehicle2(ArrayList<Vehicle> vehicleList){
+    /**
+     * Returns information of each vehicle in the tour
+     *
+     * @param   vehicleList   the location LinkedList.
+     * @return  the information  (route taken, cost, and capacity) of each vehicle
+     */
+    public String displayVehicle(ArrayList<Vehicle> vehicleList){
         for (int i = 1; i <= vehicleList.size(); i++) {
             sb.append("\nVehicle " + i +"\n" + vehicleList.get(i-1) + "\n");
         }
         return sb.toString();
     }
 
-    ArrayList<Integer> bestPath;
-    double bestCost;
-    Instant startTime;
+    /**
+     * Returns the route for each vehicle after it performs the Depth-First Search (DFS) algorithm
+     *
+     * @return the tour that consists of different vehicle using different routes.
+     */
     public ArrayList<Vehicle> dfs(){
         bestCost = Double.POSITIVE_INFINITY;
         bestPath = new ArrayList<>();
@@ -341,11 +184,19 @@ public class Graph extends Thread{
         //display output
         sb = new StringBuilder();
         sb.append("Basic Simulation Tour\nTour Cost: " + bestCost + "\n");
-        displayVehicle2(vehicleList);
+        displayVehicle(vehicleList);
 
         return vehicleList;
     }
 
+    /**
+     * Returns the tour after it examines the capacity of each vehicle that can be used to visit the customer
+     *
+     * @param   node   the ID of the node
+     * @param  adjList the neighbours of the location
+     * @param  tempPath the route sequence
+     * @return the best tour after it recursively explores the tree
+     */
     public ArrayList<Integer> depthFirstSearch(Integer node,ArrayList<Integer> adjList[] ,ArrayList<Integer> tempPath){
         tempPath.add(node);
         if(node!=0)
@@ -401,6 +252,11 @@ public class Graph extends Thread{
         return bestPath;
     }
 
+    /**
+     * Returns the route for each vehicle after it performs the greedy search algorithm
+     *
+     * @return the tour that consists of different vehicle using different routes.
+     */
     public ArrayList<Vehicle> greedySearch(){
         int currentLoad;
         tourCost=0;
@@ -414,8 +270,6 @@ public class Graph extends Thread{
 
         int i=0;  //start from depot
         while(!completeVisited()) {  //if true , greedyList has been generated, can terminate
-            //double [] array=new double[adjMatrix.length];//to compare their distance from i node
-
             double shortest = Double.POSITIVE_INFINITY;
             Customer tempCustomer = (Customer) c.get(1);
             for (int j = 1; j < adjMatrix.length; j++) { //to get shortest distance
@@ -434,20 +288,26 @@ public class Graph extends Thread{
             greedyList.add(tempCustomer);
             tempCustomer.wasVisited=true;
 
-            i = tempCustomer.id; //got error
+            i = tempCustomer.id;
         }
-        for ( i = 1; i < c.size(); i++) { //resetting all to false to create connections later
+        for ( i = 1; i < c.size(); i++) {
             ((Customer)(c.get(i))).wasVisited=false;
         }
         tourCost=vehicleDistribution(greedyList);
+
         //display output
         sb = new StringBuilder();
         sb.append("Greedy Simulation Tour\nTour Cost: " + tourCost + "\n");
-        displayVehicle2(vehicleList);
+        displayVehicle(vehicleList);
         resetVisited();
         return vehicleList;
     }
 
+    /**
+     * Returns the route for each vehicle after it performs the Best First Search algorithm
+     *
+     * @return the tour that consists of different vehicle using different routes.
+     */
     public ArrayList<Vehicle> bestFirstSearch(){
         tourCost = 0;
         ArrayList<Location> closed = new ArrayList<>(); //a list storing visited by not yet expand node
@@ -490,11 +350,16 @@ public class Graph extends Thread{
         //display output
         sb = new StringBuilder();
         sb.append("Best First Search Simulation Tour\nTour Cost: " + tourCost + "\n");
-        displayVehicle2(vehicleList);
+        displayVehicle(vehicleList);
         resetVisited();
         return vehicleList;
     }
 
+    /**
+     * Returns the route for each vehicle after it performs the A* Search algorithm
+     *
+     * @return the tour that consists of different vehicle using different routes.
+     */
     public ArrayList<Vehicle>  aStarSearch() {
         tourCost = 0;
         ArrayList<Location> open = new ArrayList<>(); //keeps all nodes that are discovered but not yet expanded
@@ -559,11 +424,18 @@ public class Graph extends Thread{
         //display output
         sb = new StringBuilder();
         sb.append("A* Search Simulation Tour\nTour Cost: " + tourCost + "\n");
-        displayVehicle2(vehicleList);
+        displayVehicle(vehicleList);
         resetVisited();
         return vehicleList;
     }
 
+    /**
+     * Returns the tour cost that is the summation of routes taken by each vehicle. It distributes the list of locations based on the vehicle maximum capacity and customer demand size,
+     * makes into linked list, then stores in vehicleList.
+     *
+     * @param   list   the list of locations
+     * @return the best tour after it recursively explores the tree
+     */
     public double vehicleDistribution(ArrayList<Location> list){
         tourCost=0;
         linkedList.clear();
@@ -608,14 +480,22 @@ public class Graph extends Thread{
         return tourCost;
     }
 
-
-
+    /**
+     * Sets the wasVisited status of all locations to {@code false}
+     */
     public void resetVisited(){
         for (int i = 1; i < c.size() ; i++) {
             ((Customer)c.get(i)).wasVisited = false;
         }
     }
 
+    /**
+     * Returns the tour that conveys information about each route cost for each vehicle
+     *
+     * @param   level   number of levels for MCTS search tree
+     * @param   iterations   number of nodes in every level
+     * @return the tour of the MCTS algorithm
+     */
     public Tour mctsSearch(int level, int iterations){
         Instant startTime = Instant.now();
         if(level==0){
@@ -635,6 +515,11 @@ public class Graph extends Thread{
         return best_tour;
     }
 
+    /**
+     * To update policy based on {@code a_tour}
+     * @param   a_tour   tour that holds information about tour cost based on all routes
+     * @param   level   number of levels for MCTS search tree
+     */
     public void adapt(Tour a_tour, int level){
         ArrayList<Location> visited = new ArrayList<>();
         //for every route in tour
@@ -664,6 +549,11 @@ public class Graph extends Thread{
         }
     }
 
+    /**
+     * Returns the tour that searhces for routes for the tour according to the policy.
+     *
+     * @return the tour
+     */
     public Tour rollout(){
         Location currentStop;
         Location nextStop;
@@ -717,6 +607,13 @@ public class Graph extends Thread{
         return new_tour;
     }
 
+    /**
+     * Returns the {@code Location} after it decides which node to go next from current node based on the policy
+     *
+     * @param   currentStop   current Location at
+     * @param   possible_successors   possible move that can be made from current node
+     * @return next move taken by the {@code currentStop}
+     */
     public Location select_next_move(Location currentStop, ArrayList<Location> possible_successors){ //possible successors = {0,1,2,3,4}
         double[] probability = new double[possible_successors.size()];
         double sum = 0;
